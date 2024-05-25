673b11efa40fe2320c1eec9d0de125b132119d39
albertsousa54 <albert.sousa@hotmail.com>
2024-05-25 13:23:53 -0300

    Add New Projet CONTA-BANCO And changes in file anotacoes.dm

---
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -0,0 +1,16 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        
+        //ETAPAS QUE DEVEM SER REALIZADAS NESSE DESAFIO.
+
+        //1º CONHECER E IMPORTAR A CLASSE SCANNER
+
+        //2º EXIBIR AS MENSAGENS PARA O NOSSO USUÁRIO
+
+        //3º OPTER/CAPTURAR ATRAVES DO SCANNER OS VALORES DIGITADOS
+
+        //4º E ULTIMO EXIBIR A MENSAGEM DE OCNTA CRIADA.
+
+
+    }
+}
